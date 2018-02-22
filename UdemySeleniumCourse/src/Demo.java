338575f7600c9102034de4ecaf174a3a6f016a32
1459751 <1459751@22HW020539.SOAM.TCS.com>
2018-02-22 16:46:28 -0600

    Continues whith the course and start with dropdown.

--- UdemySeleniumCourse/src/Demo.java
+++ UdemySeleniumCourse/src/Demo.java
@@ -6,15 +6,41 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "/home/zerep08/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\1459751\\Documents\\Libraries\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-		
+		/*
 		driver.get("https://www.facebook.com/");
 		////tagname[@attribute = 'value'] - xpath
 		
 		// tagname[attribute = 'value'] - cssSelector
 		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("Hola que tal");
+		
+		*/
+		
+		
+		/*
+		Select web elements with regular expresions
+		driver.get("http://www.rediff.com/");
+		driver.findElement(By.cssSelector("a[title*='Sign in']")).click();
+		driver.findElement(By.xpath("//input[@id='login1']")).sendKeys("Hello");
+		driver.findElement(By.cssSelector("input#password")).sendKeys("1234");
+		driver.findElement(By.xpath("//input[contains(@name ='procee')]")).click();
+		*/
+		
+		
+		//Parent to child xpath
+		driver.get("https://www.google.com.mx");
+		driver.findElement(By.xpath("//*[@id=\'lst-ib\']")).sendKeys("Hello parent");
+		
+		//*[@id="lst-ib"]
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ UdemySeleniumCourse/src/Dropdown.java
@@ -0,0 +1,13 @@
+
+public class Dropdown {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\1459751\\Documents\\Libraries\\chromedriver_win32\\chromedriver.exe");
+
+		
+		
+		
+		
+	}
+
+}
